402dc6c0c158fb5969bf6883c7d62ace4c415a42
Andrew <andrew.j.price@outlook.com>
2019-03-23 23:21:59 -0600

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Andrew Price on 3/23/19.
+//  Copyright © 2019 Andrew Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
